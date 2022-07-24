b83053b7e39e357012eb167f7ef059ffe2cab8ec
Z903 <z903tk@gmail.com>
2022-07-24 14:01:05 -0700

    Initial Commit

---
+++ DungeonSolve.py
@@ -0,0 +1,214 @@
+import collections, copy, itertools, json
+
+class State():
+    def __init__(self, rows, cols, mons, trea, **kwarg):
+        self.rows = rows
+        self.cols = cols
+        self.board = list()
+        self.treaure = trea
+        self.monsters = mons
+        
+        # The number of cols is the width
+        self.width = len(self.cols)
+        self.height = len(self.rows)
+
+        for _ in range(2): self.board.append(["X"] * (self.width + 4))
+        for _ in range(self.height): self.board.append(["X","X"] + ["."] * self.width + ["X","X"])
+        for _ in range(2): self.board.append(["X"] * (self.width + 4))
+
+        for m in mons:
+            self.board[m[1] + 2][m[0] + 2] = "O"
+        for t in trea:
+            self.board[t[1] + 2][t[0] + 2] = "T"
+
+    def clone(self):
+        c = copy.copy(self)
+        c.board = copy.deepcopy(self.board)
+        return c
+
+    def transposed(self):
+        return list(map(list, zip(*self.board)))
+
+    def show(self):
+        print("." + "".join(str(x) for x in self.cols))
+        for r in range(self.height):
+            print("{}{}".format(self.rows[r], "".join(str(x) for x in self.board[r + 2][2:-2])))
+        print("")
+
+    def check_treasure_early(self, treaure):
+        x = treaure[0] + 2
+        y = treaure[1] + 2
+
+        def a(x,y):
+            if self.board[y - 1][x - 1] not in (".", "T"): return False
+            if self.board[y - 0][x - 1] not in (".", "T"): return False
+            if self.board[y + 1][x - 1] not in (".", "T"): return False
+            if self.board[y - 1][x - 0] not in (".", "T"): return False
+            if self.board[y - 0][x - 0] not in (".", "T"): return False
+            if self.board[y + 1][x - 0] not in (".", "T"): return False
+            if self.board[y - 1][x + 1] not in (".", "T"): return False
+            if self.board[y - 0][x + 1] not in (".", "T"): return False
+            if self.board[y + 1][x + 1] not in (".", "T"): return False
+            return True
+    
+        result = set()
+        for (ox,oy) in [(-1,-1), (-1,0), (-1,1),(0,-1), (0,0), (0,1),(1,-1), (1,0), (1,1)]:
+            if a(x + ox, y + oy):
+                result.add((x + ox, y + oy))
+        return result
+
+    def check(self, row):
+        """
+        Check from 0 up to `row`
+        """
+        solved = True
+  
+        # Check each row
+        for i in range(self.height):
+            temp = sum(1 for x in self.board[i + 2] if x == "X") - 4
+            if temp > self.rows[i]: return False
+            if temp < self.rows[i]: solved = False
+   
+        # Check each col
+        bt = self.transposed()
+        for i in range(self.width):
+            temp = sum(1 for x in bt[i + 2] if x == "X") - 4
+            if temp > self.cols[i]: return False
+            if temp < self.cols[i]: solved = False
+    
+        # Dead end check
+        for y in range(2, row + 3):
+            for x in range(2, self.width + 2):
+                if self.board[y][x] == ".":
+                    t = 0
+                    if self.board[y - 1][x] == "X": t += 1
+                    if self.board[y + 1][x] == "X": t += 1
+                    if self.board[y][x - 1] == "X": t += 1
+                    if self.board[y][x + 1] == "X": t += 1
+                    if (t >= 3):
+                        return False
+
+        #for pos in range(8 * (row + 1)):
+        #    x = int(pos % 8) + 2
+        #    y = int(pos / 8) + 2
+        #    if self.board[y][x] == ".":
+        #        t = 0
+        #        if self.board[y - 1][x] == "X": t += 1
+        #        if self.board[y + 1][x] == "X": t += 1
+        #        if self.board[y][x - 1] == "X": t += 1
+        #        if self.board[y][x + 1] == "X": t += 1
+        #        if (t >= 3):
+        #            return False
+
+        # Early treasure check
+        treasures = list()
+        for t in self.treaure:
+            treasure = self.check_treasure_early(t)
+            if treasure == set():
+                return False
+            treasures.append(treasure)
+
+        treasure_tiles = set()
+        for t in treasures:
+            treasure_tiles = treasure_tiles.union(t)
+
+        # Check for 2x2
+        for y in range(3, row + 3):
+            for x in range(3, self.width + 2):
+                t = 0
+                if self.board[y - 1][x - 0] == ".": t += 1
+                if self.board[y - 1][x - 1] == ".": t += 1
+                if self.board[y - 0][x - 0] == ".": t += 1
+                if self.board[y - 0][x - 1] == ".": t += 1
+                # exempt 2x2 areas in the treasure room
+                if t == 4 and len(treasure_tiles.intersection({(x,y), (x-1,y), (x,y-1), (x-1,y-1)})) == 0:
+                    return False
+
+        #for pos in range(1, 8 * (row + 1)):
+        #    x = int(pos % 8) + 2
+        #    y = int(pos / 8) + 2
+        #    t = 0
+        #    if self.board[y - 1][x - 0] == ".": t += 1
+        #    if self.board[y - 1][x - 1] == ".": t += 1
+        #    if self.board[y - 0][x - 0] == ".": t += 1
+        #    if self.board[y - 0][x - 1] == ".": t += 1
+        #    # exempt 2x2 areas in the treasure room
+        #    if t == 4 and len(treasure_tiles.intersection({(x,y), (x-1,y), (x,y-1), (x-1,y-1)})) == 0:
+        #        return False
+
+        # Check that monsters have an exit
+        for m in self.monsters:
+            x = m[0] + 2
+            y = m[1] + 2
+            if not any(self.board[y + ox][x - oy] == "." for ox,oy in [(-1,0), (1,0), (0,-1), (0,1)]):
+                return False
+
+        # If the board is partially solved return None
+        if solved == False:
+            return None
+
+        # Checks below here requre the board to be complete
+
+        # Check that monsters have 3 walls
+        for m in self.monsters:
+            x = m[0] + 2
+            y = m[1] + 2
+            if sum(self.board[y + ox][x - oy] == "X" for ox,oy in [(-1,0), (1,0), (0,-1), (0,1)]) != 3:
+                return False
+
+        # Check treasure room walls/exit
+        for treasure in treasures:
+            if len(treasure) != 1:
+                return False
+            t = next(iter(treasure))
+            x = t[0]
+            y = t[1]
+
+            inner = collections.Counter(self.board[y + oy][x + ox] for ox,oy in [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)])
+            walls = collections.Counter(self.board[y + oy][x + ox] for ox,oy in [(-1,-2),(0,-2),(1,-2),(2,-1),(2,0),(2,1),(1,2),(0,2),(-1,2),(-2,1),(-2,0),(-2,-1)])
+
+            if inner["."] != 8 or inner["T"] != 1 or walls["X"] != 11 or walls["."] != 1:
+                return False
+
+        # Note that we never actually check that all paths connect; we may want to do this in the future
+
+        # We have found a solution
+        return True
+
+    def solve(self, row = 0):
+        if row >= self.height: return
+
+        y = row + 2
+        open_spaces = (x for x in range(2, self.width + 2) if self.board[y][x] == ".")
+        for s in itertools.combinations(open_spaces, self.rows[row]):
+
+            # Set the selected open spaces to X
+            for x in s:
+                self.board[y][x] = "X"
+
+            # Check board state
+            ok = self.check(row)
+
+            if ok == True:
+                # We found a solution yield a clone
+                yield self.clone()
+
+            # Recurse to the next line
+            if ok == None:
+                for x in self.solve(row + 1):
+                    # Yield solutions out
+                    yield x
+                    
+            # Reset the selected open spaces to "."
+            for x in s:
+                self.board[y][x] = "."
+
+puzzles = {}
+with open("puzzles.json") as f:
+    puzzles = json.load(f)["puzzles"]
+for p in puzzles.values():
+    print p["name"] + "\n"
+    state = State(**p)
+    state.show()
+    for solution in state.solve():
+        solution.show()
---
+++ readme.md
@@ -0,0 +1,5 @@
+A recursive solver for Dungeons and Diagrams
+
+https://www.reddit.com/r/puzzles/comments/d72zg1/advanced_dungeons_and_diagrams_map_making_logic/
+
+This solver was created to see if there was more then one solution to the puzzles in [Last Call BBS](https://www.zachtronics.com/last-call-bbs/).
